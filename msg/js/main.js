ec4a220e88d9b1c25907620ab1b794705e23b5e7
pechka1 <staspechka1@gmail.com>
2019-02-03 22:03:38 +0200

    msg

--- msg/js/main.js
+++ msg/js/main.js
@@ -1,9 +1,13 @@
+//Dropdown menu
 $(document).ready(function() {
 	$('.dropdown_sidebar').on('click',function(){
 		$(this).toggleClass('open');
 	});
+//BLOCK INFO
+	$('.info_block').on('click',function(){
+		$('.block_six_response_mask').addClass('close');
+	});
 //Anchor
-
 	$('a.anchor').on('click', function(){
 		var id = $(this).attr('href');
 		$('html, body').animate({scrollTop: $(id).offset().top},1000)
@@ -11,6 +15,7 @@ $(document).ready(function() {
 });
 
 $('#comment').attr('placeholder', 'Комментарий: ' + '\n' + 'Могу ли я записаться на 16:00 во вторник?');
+
 
 
 
