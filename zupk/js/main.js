d527d1d9091615db4d72ece46e61b0e57bf70572
pechka1 <43072645+pechka1@users.noreply.github.com>
2020-02-05 19:03:38 +0200

    zupk
    ver:1

---
+++ zupk/js/main.js
@@ -0,0 +1,13 @@
+let types = document.querySelectorAll('section.about_us .type .item button');
+
+for(let type of types){
+	type.addEventListener('click', function(){
+		if(type.parentNode.querySelector('.list').classList.contains('show')){
+			type.querySelector('span').innerHTML = 'Переглянути спеціалістів';
+		}else{
+			type.querySelector('span').innerHTML = 'Закрити список';
+		}
+		type.classList.toggle('show');
+		type.parentNode.querySelector('.list').classList.toggle('show');
+	})
+}
