9425054e91a9e7aa88717678275489df9ded3f51
pechka1 <43072645+pechka1@users.noreply.github.com>
2020-02-08 15:54:18 +0200

    ver 7
    + list animation for peson

--- zupk/js/main.js
+++ zupk/js/main.js
@@ -2,13 +2,15 @@ let types = document.querySelectorAll('section.about_us .type .item button');
 
 for(let type of types){
 	type.addEventListener('click', function(){
-		if(type.parentNode.querySelector('.list').classList.contains('show')){
-			type.querySelector('span').innerHTML = 'Переглянути спеціалістів';
-		}else{
-			type.querySelector('span').innerHTML = 'Сховати список';
-		}
 		type.classList.toggle('show');
 		type.parentNode.querySelector('.list').classList.toggle('show');
+    if (type.parentElement.querySelector('.list').style.maxHeight){
+      type.parentElement.querySelector('.list').style.maxHeight = null;
+			type.querySelector('span').innerHTML = 'Переглянути спеціалістів';
+    } else {
+			type.querySelector('span').innerHTML = 'Сховати список';
+      type.parentElement.querySelector('.list').style.maxHeight = type.parentElement.querySelector('.list').scrollHeight + "px";
+    } 
 	})
 }
 
@@ -19,11 +21,18 @@ let full_text = more.parentElement.querySelector('.full_text').innerHTML;
 
 more.addEventListener('click',function(){
 	more.classList.toggle('show');
+	// if(more.classList.contains('show')){
+	// }else{
+	// }
+
 	if(more.classList.contains('show')){
 		more.querySelector('span').innerHTML ='заховати';
+		// more.parentElement.querySelector('.main_text').style.maxHeight = null;
 		more.parentElement.querySelector('.main_text').innerHTML = full_text;
+		// more.parentElement.querySelector('.main_text').classList.add('show');
 	}else{
 		more.querySelector('span').innerHTML ='читати';
+		// more.parentElement.querySelector('.main_text').style.maxHeight = more.parentElement.querySelector('.main_text').scrollHeight + "px";
 		more.parentElement.querySelector('.main_text').innerHTML = full_text.substring(0,193) + '...';
 	}
 });
