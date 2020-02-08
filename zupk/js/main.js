be677dc7a2cd872496236c2fba22778033128772
pechka1 <43072645+pechka1@users.noreply.github.com>
2020-02-08 14:31:51 +0200

    ver 6
    + personal form
    + personal show more/less

--- zupk/js/main.js
+++ zupk/js/main.js
@@ -11,3 +11,19 @@ for(let type of types){
 		type.parentNode.querySelector('.list').classList.toggle('show');
 	})
 }
+
+
+let more = document.querySelector('section.about_us .person .more');
+let full_text = more.parentElement.querySelector('.full_text').innerHTML;
+		more.parentElement.querySelector('.main_text').innerHTML = full_text.substring(0,193) + '...';
+
+more.addEventListener('click',function(){
+	more.classList.toggle('show');
+	if(more.classList.contains('show')){
+		more.querySelector('span').innerHTML ='заховати';
+		more.parentElement.querySelector('.main_text').innerHTML = full_text;
+	}else{
+		more.querySelector('span').innerHTML ='читати';
+		more.parentElement.querySelector('.main_text').innerHTML = full_text.substring(0,193) + '...';
+	}
+});
