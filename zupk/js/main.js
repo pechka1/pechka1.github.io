314923038fbdbff22b6d8bcf8a552895c157f78e
pechka1 <43072645+pechka1@users.noreply.github.com>
2020-02-12 10:39:41 +0200

    ver 15
    adapt

--- zupk/js/main.js
+++ zupk/js/main.js
@@ -3,10 +3,10 @@ let anchors = document.querySelectorAll('.anchors a , a.anchors');
 
 for(let anchor of anchors){
 	anchor.addEventListener('click', function(e){
-		 e.preventDefault();
+		e.preventDefault();
 		let href = document.getElementById(anchor.getAttribute('href').replace('#',''));
 		console.log(href);
-  	href.scrollIntoView({block: "start", behavior: "smooth"});
+		href.scrollIntoView({block: "start", behavior: "smooth"});
 	})
 }
 document.querySelector('.burger_menu_right').addEventListener('click', function(){
@@ -19,20 +19,20 @@ for(let type of types){
 	type.addEventListener('click', function(){
 		type.classList.toggle('show');
 		type.parentNode.querySelector('.list').classList.toggle('show');
-    if (type.parentElement.querySelector('.list').style.maxHeight){
-      type.parentElement.querySelector('.list').style.maxHeight = null;
+		if (type.parentElement.querySelector('.list').style.maxHeight){
+			type.parentElement.querySelector('.list').style.maxHeight = null;
 			type.querySelector('span').innerHTML = 'Переглянути спеціалістів';
-    } else {
+		} else {
 			type.querySelector('span').innerHTML = 'Сховати список';
-      type.parentElement.querySelector('.list').style.maxHeight = type.parentElement.querySelector('.list').scrollHeight + "px";
-    } 
+			type.parentElement.querySelector('.list').style.maxHeight = type.parentElement.querySelector('.list').scrollHeight + "px";
+		} 
 	})
 }
 
 
 let more = document.querySelector('section.about_us .person .more');
 let full_text = more.parentElement.querySelector('.full_text').innerHTML;
-		more.parentElement.querySelector('.main_text').innerHTML = full_text.substring(0,193) + '...';
+more.parentElement.querySelector('.main_text').innerHTML = full_text.substring(0,193) + '...';
 
 more.addEventListener('click',function(){
 	more.classList.toggle('show');
